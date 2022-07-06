61f558f269266a0e7b1482b328bd1c2afedfdfd5
dmquinn <danielmurphyberling@gmail.com>
2022-07-06 23:04:45 +0200

    mock user data added

--- components/Stories.js
+++ components/Stories.js
@@ -1,28 +1,43 @@
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 const Stories = () => {
   const baseUrl = "https://ghibliapi.herokuapp.com/";
   const [hasError, setErrors] = useState(false);
-  const [movies, setMovies] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/user")
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        setMovies(res);
+  const fetcher = async () => {
+    const res = await fetch("https://reqres.in/api/users?per_page=12");
+    const json = await res.json();
+    const userResult = json.data;
+    setUsers(
+      userResult.map((user) => {
+        return {
+          isActive: Math.random() < 0.5, // 50/50 chance of isActive
+          ...user,
+        };
       })
-      .catch((err) => setErrors(true));
-  }, []);
-
+    );
+  };
   useEffect(() => {
-    fetchData();
+    fetcher();
   }, []);
-
   return (
-    <div>
-      {suggestions &&
-        suggestions.map((suggestion, i) => (
-          <li key={i}>{suggestion.user.firstName}</li>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border border-gray-200 rounded-sm overflow-x-scroll">
+      {!!users &&
+        users.map((suggestion, i) => (
+          <div key={i}>
+            <Image
+              src={suggestion.avatar}
+              alt="Picture of the author"
+              width={100}
+              height={100}
+              fit="fill"
+            />
+            <h1>
+              {suggestion.first_name} {suggestion.last_name}
+            </h1>
+          </div>
         ))}
     </div>
   );
