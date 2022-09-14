58fc886346231fc5571a5f2d2621e580b30dadf5
dmquinn <danielmurphyberling@gmail.com>
2022-09-14 15:33:02 +0200

    added typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
